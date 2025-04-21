8c5a200ff6ce72970b50bb6ff7089510d7d74176
Steven Wilson <2165238+mendeni@users.noreply.github.com>
2025-04-21 13:06:42 -0400

    Updated build scipt with example for MacOS x86_64

--- build.sh
+++ build.sh
@@ -2,11 +2,16 @@
 
 set -x
 
-make clean
-
+# Works on MacOS arm64 (M1 chip)
 # ./configure CC="gcc -arch x86_64 -arch arm64" --enable-dred --enable-deep-plc
 ./configure CC="gcc -arch x86_64 -arch arm64" --enable-deep-plc
 # ./configure CC="gcc -arch x86_64 -arch arm64"
+
+# We need to --disable-rtcd on MacOS x86_84 due to this issue:
+# https://gitlab.xiph.org/xiph/opus/-/issues/2361
+# ./configure CC="gcc -arch x86_64 -arch arm64" --enable-deep-plc --disable-rtcd
+
+make clean
 
 make
 
